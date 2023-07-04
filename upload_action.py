43186e1c3eb7b77328d2ad1ccb236a561e5bc446
Keith Burroughs <51885619+keithhb33@users.noreply.github.com>
2023-07-03 20:06:42 -0400

    Update upload_action.py

--- upload_action.py
+++ upload_action.py
@@ -4,7 +4,7 @@ import shlex
 
 def upload_to_youtube(movietitle):
     # Set the command
-    command = f'python3 youtube_upload.py --file="output/{movietitle}.mp4" --title="{movietitle} - AWFUL MOVIE SHORTS" --description="Like, comment, and subscribe for more top tier movie content." --category="22" --privacyStatus="public"'
+    command = f'python3 youtube_upload.py --file="output/{movietitle}.mp4" --title="{movietitle}" --description="Like, comment, and subscribe for more top tier movie content." --category="22" --privacyStatus="public"'
     
     # Use shlex to split the command string into a list
     args = shlex.split(command)
